a87b9acd6d82e062b719bdeb3f4ad0a84b0ca5e3
kennymaina <victormurigi@outlook.com>
2018-08-25 20:26:24 +0300

    change of dice.html to index.html

--- js/scripts.js
+++ js/scripts.js
@@ -2,23 +2,7 @@ var scores, roundScore, activePlayer,gamePlaying;
 var btnRoll = document.querySelector('.btn-roll');
 var btnHold = document.querySelector('.btn-hold');
 
-//dice images
-var diceimgs = {
- diceimg01: "https://cdn.pbrd.co/images/70YJMCVVR.png",
- diceimg02: 'https://cdn.pbrd.co/images/711lemsMX.png',
- diceimg03: "https://cdn.pbrd.co/images/711NjfjV5.png",
- diceimg04: "https://cdn.pbrd.co/images/712dK3C2z.png",
- diceimg05: "https://cdn.pbrd.co/images/70Zqc4icX.png",
- diceimg06: "https://cdn.pbrd.co/images/712DzRw22.png",
- diceimg11: "https://cdn.pbrd.co/images/713n3lHQN.png",
- diceimg12: 'https://cdn.pbrd.co/images/713JSMJDr.png',
- diceimg13: "https://cdn.pbrd.co/images/HvoZO4Gb.png",
- diceimg14: "https://cdn.pbrd.co/images/HvqN3Kjq.png",
- diceimg15: "https://cdn.pbrd.co/images/714IXBStH.png",
- diceimg16: "https://cdn.pbrd.co/images/714ZovsdD.png"
-};
-init();
-
+/
 
 
 document.querySelector('.btn-roll').addEventListener('click', function(){
